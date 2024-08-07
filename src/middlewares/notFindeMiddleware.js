1c3221b90c6ec65c51252655d77b9254f0e43c7d
wordisstuff <wordisstuff@gmail.com>
2024-08-07 11:28:19 -0500

    3hw-v0.2

--- src/middlewares/notFindeMiddleware.js
+++ src/middlewares/notFindeMiddleware.js
@@ -1,5 +1,6 @@
-// import { Httperror } from 'http-errors';
+import createHttpError from 'http-errors';
 
-export const notFindeMiddleware = (req, res) => {
-    res.status(404).send({ message: 'Not found' });
+export const notFindeMiddleware = (req, res, next) => {
+    // res.status(404).send({ message: 'Not found' });
+    next(createHttpError(404, 'Route not found.'));
 };
