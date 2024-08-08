900667df8cd93382ea094fcd09c1b696f93b4a8f
wordisstuff <wordisstuff@gmail.com>
2024-08-08 18:38:45 -0500

    Update contactValidateSchemas.js

--- src/validation/contactValidateSchemas.js
+++ src/validation/contactValidateSchemas.js
@@ -4,7 +4,7 @@ export const createContactSchema = Joi.object({
     name: Joi.string().min(3).max(20).required(),
     email: Joi.string().min(3).max(20).required(),
     phoneNumber: Joi.string().min(3).max(20).required(),
-    isFavorite: Joi.boolean().default(false),
+    isFavourite: Joi.boolean().default(false),
     conactType: Joi.string()
         .valid('work', 'personal', 'home')
         .default('personal')
@@ -15,7 +15,7 @@ export const updateContactSchema = Joi.object({
     name: Joi.string().min(3).max(20),
     email: Joi.string().min(3).max(20),
     phoneNumber: Joi.string().min(3).max(20),
-    isFavorite: Joi.boolean().default(false),
+    isFavourite: Joi.boolean().default(false),
     conactType: Joi.string()
         .valid('work', 'personal', 'home')
         .default('personal'),
