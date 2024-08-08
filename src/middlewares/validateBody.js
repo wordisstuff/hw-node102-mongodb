dccd76d7a8ecda0eecbd54cfa8677091c3eb3074
wordisstuff <wordisstuff@gmail.com>
2024-08-08 00:18:29 +0000

    hw4-v0.2

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -3,6 +3,9 @@ import createHttpError from "http-errors";
 const validateBody = (schema) => (req,res,next)=>{
     const {error} = schema.validate(req.body, {abortEarly: false});
     if(error){
-        next(createHttpError())
-    }
-}
+        next(createHttpError(400,{ data:error.details}))
+    };
+    next();
+};
+
+export default validateBody;
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -10,13 +10,16 @@ import {
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { jsonParser } from '../constants/constants.js';
 import isValidId from '../middlewares/isValidId.js';
+import validateBody from '../middlewares/validateBody.js';
+
+import { createContactSchema,updateContactSchema } from '../validation/contactValidateSchemas.js';
 
 const contactsRouter = Router();
 
 contactsRouter.get('/', ctrlWrapper(getAllContactsController));
 contactsRouter.get('/:id',isValidId, ctrlWrapper(getContactByIdController));
-contactsRouter.post('/', jsonParser, ctrlWrapper(createContactController));
-contactsRouter.patch('/:id',isValidId, jsonParser, ctrlWrapper(updateContactController));
+contactsRouter.post('/', jsonParser,validateBody(createContactSchema), ctrlWrapper(createContactController));
+contactsRouter.patch('/:id',isValidId, jsonParser,validateBody(updateContactSchema), ctrlWrapper(updateContactController));
 contactsRouter.delete('/:id',isValidId, ctrlWrapper(deleteContactController));
 
 export default contactsRouter;
