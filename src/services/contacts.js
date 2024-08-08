e76a595d9f01d433ef14fc2f9b8a7b6310cf22e7
wordisstuff <wordisstuff@gmail.com>
2024-08-08 05:43:39 +0000

    hw4-v0.4

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,6 +1,16 @@
 import { Contact } from '../db/models/contacts.js';
+import {calculatePaginationData} from '../utils/calculatePaginationData.js'
+ 
+export const getContacts = ({page,perPage}) => {
+        const skip = (page-1)*perPage;
 
-export const getContacts = () => Contact.find();
+       const contactsQuery = Contact.find();
+       const contactsCount = Contact.find().merge(contactsQuery).countDocuments();
+       const contacts = contactsQuery.skip(skip).limit(perPage).exec();
+       const paginationData = calculatePaginationData(contactsCount,page,perPage);
+
+       return {data:contacts,...paginationData};
+    };
 export const getContactById = id => Contact.findById(id);
 export const postContact = body => Contact.create(body);
 export const patchContact = (id, body) =>
