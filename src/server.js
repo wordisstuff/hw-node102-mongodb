57f17d7b0707ab138efb9c2b941152dd9b12321b
wordisstuff <wordisstuff@gmail.com>
2024-08-09 01:11:01 +0000

    hw4-v0.8

--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ export const setupServer = () => {
     app.use(pino(pinoSettings));
     app.use(cors());
 
+    app.use('/', (req,res)=> res.send('Hello! it is home work 4 from Wordisstuff'));
     app.use('/contacts', contactsRouter);
 
     app.use(notFindeMiddleware);
