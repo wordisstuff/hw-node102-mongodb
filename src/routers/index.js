ab43bc0e29e6fbd5eba0c956d7fcc4db3ebdf839
wordisstuff <wordisstuff@gmail.com>
2024-08-22 17:00:37 -0500

    Update index.js

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import contactsRouter from './contacts.js';
 import authRouter from './auth.js';
-import { authenticate } from '../middlewares/authenticate.js';
+// import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
 
-router.use('/contacts', authenticate, contactsRouter);
+router.use('/contacts', 
+           // authenticate
+           contactsRouter);
 router.use('/auth', authRouter);
 
 export default router;
